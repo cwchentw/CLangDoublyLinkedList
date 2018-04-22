e06b42a9c071035d6c649e6571ea14f9c5bf83f9
Michael Chen <cwchen123@gmail.com>
2018-04-22 23:43:25 +0000

    Refactor some code

--- test_list.c
+++ test_list.c
@@ -5,57 +5,28 @@
 #include "test_manipulation.h"
 #include "test_traversal.h"
 
+#define TEST(test) { \
+        if (!(test)) { \
+            fprintf(stderr, "%s on %d: Failed on %s", __FILE__, __LINE__, #test); \
+            exit(EXIT_FAILURE); \
+        } \
+    }
+
 int main()
 {
-    if (!test_list_is_empty()) {
-        perror("Failed on test_list_is_empty");
-        exit(EXIT_FAILURE);
-    }
+    // Test list manipulation.
+    TEST(test_list_is_empty());
+    TEST(test_list_unshift());
+    TEST(test_list_push());
+    TEST(test_list_shift());
+    TEST(test_list_insert_when());
+    TEST(test_list_pop());
     
-    if (!test_list_unshift()) {
-        perror("Failed on test_list_unshift");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_push()) {
-        perror("Failed on test_list_push");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_shift()) {
-        perror("Failed on test_list_shift");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_insert_when()) {
-        perror("Failed on test_list_when()");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_pop()) {
-        perror("Failed on test_list_pop");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_iter()) {
-        perror("Failed on test_list_iter");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_any()) {
-        perror("Failed on test_list_any");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_all()) {
-        perror("Failed on test_list_all");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!test_list_map()) {
-        perror("Failed on test_list_map");
-        exit(EXIT_FAILURE);
-    }
+    // Test list traversal.
+    TEST(test_list_iter());
+    TEST(test_list_any());
+    TEST(test_list_all());
+    TEST(test_list_map());
     
     return 0;
 }
