a8db5121e793f16c368b56f485e72a9a35357b71
Michael Chen <cwchen123@gmail.com>
2018-12-21 07:03:49 +0800

    Migrate the project

--- tests/test_list.c
+++ tests/test_list.c
@@ -1,7 +1,43 @@
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "list.h"
+#include "test_manipulation.h"
+#include "test_traversal.h"
+
+#define TEST(test) { \
+        if (!(test)) { \
+            fprintf(stderr, "%s on %d: Failed on %s", __FILE__, __LINE__, #test); \
+            exit(EXIT_FAILURE); \
+        } \
+    }
+
+int main()
+{
+    // Test list state.
+    TEST(test_list_is_empty());
+
+    // Test list getters.
+    TEST(test_list_at());
+
+    // Test list setters.
+    TEST(test_list_set_at());
+
+    // Test list manipulation.
+    TEST(test_list_unshift());
+    TEST(test_list_push());
+    TEST(test_list_shift());
+    TEST(test_list_insert_when());
+    TEST(test_list_pop());
+
+    // Test list traversal.
+    TEST(test_list_iter());
+    TEST(test_list_any());
+    TEST(test_list_all());
+    TEST(test_list_map());
+
+    return 0;
+}
 
 bool test_list_is_empty(void)
 {
@@ -401,6 +437,196 @@ bool test_list_pop(void)
 
 LIST_FREE:
     list_free(lt);
+
+    if (failed) {
+        return false;
+    }
+
+    return true;
+}
+
+bool test_list_iter(void)
+{
+    bool failed = false;
+
+    // List: 4 -> 9 -> 5 -> NULL;
+    List *lt = list_init(3, 4, 9, 5);
+    if (lt == NULL) {
+        perror("Failed to allocate List lt");
+        return false;
+    }
+
+    int arr[] = {4, 9, 5};
+    size_t i = 0;
+    // Iterate through lt.
+    for (ListIter it = list_start(lt); !list_end(it); it = list_next(it)) {
+        if (node_value(it) != arr[i]) {
+            failed = true;
+            goto LIST_FREE;
+        }
+
+        i++;
+    }
+
+LIST_FREE:
+    list_free(lt);
+
+    if (failed) {
+        return false;
+    }
+
+    return true;
+}
+
+// Nested function, available in GCC.
+bool is_even(int n)
+{
+    return n % 2 == 0;
+}
+
+bool test_list_any(void)
+{
+    bool failed = false;
+
+    List *lp = list_new();
+    if (lp == NULL) {
+        perror("Failed to allocate List lp");
+        return false;
+    }
+
+    if (list_any(lp, is_even) != false) {
+        failed = true;
+        goto LIST_P_FREE;
+    }
+
+    List *lq = list_init(5, 1, 3, 5, 7, 9);
+    if (lq == NULL) {
+        perror("Failed to allocate List lq");
+        failed = true;
+        goto LIST_P_FREE;
+    }
+
+    if (list_any(lq, is_even) != false) {
+        failed = true;
+        goto LIST_Q_FREE;
+    }
+
+    List *lr = list_init(5, 1, 2, 3, 4, 5);
+    if (lr == NULL) {
+        perror("Failed to allocate List lr");
+        failed = true;
+        goto LIST_Q_FREE;
+    }
+
+    if (list_any(lr, is_even) != true) {
+        failed = true;
+        goto LIST_R_FREE;
+    }
+
+LIST_R_FREE:
+    list_free(lr);
+LIST_Q_FREE:
+    list_free(lq);
+LIST_P_FREE:
+    list_free(lp);
+
+    if (failed) {
+        return false;
+    }
+
+    return true;
+}
+
+bool test_list_all(void)
+{
+    bool failed = false;
+
+    List *lp = list_new();
+    if (lp == NULL) {
+        perror("Failed to allocate List lp");
+        return false;
+    }
+
+    if (list_all(lp, is_even) != false) {
+        failed = true;
+        goto LIST_P_FREE;
+    }
+
+    List *lq = list_init(5, 1, 2, 3, 4, 5);
+    if (lq == NULL) {
+        perror("Failed to allocate List lq");
+        failed = true;
+        goto LIST_P_FREE;
+    }
+
+    if (list_all(lq, is_even) != false) {
+        failed = true;
+        goto LIST_Q_FREE;
+    }
+
+    List *lr = list_init(5, 2, 4, 6, 8, 10);
+    if (lr == NULL) {
+        perror("Failed to allocate List lr");
+        failed = true;
+        goto LIST_Q_FREE;
+    }
+
+    if (list_all(lr, is_even) != true) {
+        failed = true;
+        goto LIST_R_FREE;
+    }
+
+LIST_R_FREE:
+    list_free(lr);
+LIST_Q_FREE:
+    list_free(lq);
+LIST_P_FREE:
+    list_free(lp);
+
+    if (failed) {
+        return false;
+    }
+
+    return true;
+}
+
+int square(int n)
+{
+    return n * n;
+}
+
+bool test_list_map(void)
+{
+    bool failed = false;
+
+    // List p: 1 -> 2 -> 3 -> NULL
+    List *lp = list_init(3, 1, 2, 3);
+    if (lp == NULL) {
+        perror("Failed to allocate List lp");
+        return false;
+    }
+
+    // List q: 1 -> 4 -> 9 -> NULL
+    // Traverse in functional style.
+    if (!list_map(&lp, square)) {
+        perror("Failed to allocate List lq");
+        failed = true;
+        goto LIST_P_FREE;
+    }
+
+    int arr[] = {1, 4, 9};
+    size_t i = 0;
+    for (ListIter it = list_start(lp); !list_end(it); it = list_next(it)) {
+        if (node_value(it) != arr[i]) {
+            failed = true;
+            goto LIST_P_FREE;
+        }
+
+        i++;
+    }
+
+LIST_P_FREE:
+    list_free(lp);
 
     if (failed) {
         return false;
